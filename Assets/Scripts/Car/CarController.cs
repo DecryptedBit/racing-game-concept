03192d60300fee7dd1228cb54b451a5c44a09f97
Mick Veldhuis <mickveldhuis@hotmail.nl>
2020-04-22 22:46:15 +0200

    Add a basic car model and very crude controls

---
+++ Assets/Scripts/Car/CarController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float force = 500f;
+    public float torque = 500f;
+    public float gravAcceleration = 100f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float dt = Time.deltaTime;
+
+        // Update gravity
+        rb.AddForce(0, -gravAcceleration*dt, 0);
+        
+        // Turn left/right
+        if (Input.GetKey(KeyCode.A))
+            rb.AddTorque(-transform.up * torque);
+
+        if (Input.GetKey(KeyCode.D))
+            rb.AddTorque(transform.up * torque);
+        
+        // Forward/backward
+        if (Input.GetKey(KeyCode.W))
+            rb.AddForce(0, 0, force*dt);
+
+        if (Input.GetKey(KeyCode.S))
+            rb.AddForce(0, 0, -force*dt);
+    }
+}
